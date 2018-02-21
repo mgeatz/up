86815508afe621d88b810a694075c89e2b48e69d
GEATZ <geatz@geatz-air.local>
2018-02-20 21:57:29 -0600

    typed intro message

--- app/components/jumbo-search.js
+++ app/components/jumbo-search.js
@@ -1,21 +1,21 @@
 import Component from '@ember/component';
 import {computed} from '@ember/object';
-import typed from 'typed';
 
 export default Component.extend({
 
   typedStrings: [
-    "Hi there,",
-    "do you have 10 seconds?",
-    "We are in start-up mode",
-    "current phase = market feasibility",
+    "Hi there, do you have 10 seconds?",
+    "We're a new company.",
+    "Current phase = market feasibility.",
     "We need your help.",
     "Please create a simulated travel trip.",
-    "Your clicks are vital to our success.",
+    "Your clicks are a vital metric.",
     "Thank you!"
   ],
 
   search: 'az_search',
+
+  introMsg: true,
 
   bckimg: computed('search', function () {
     return this.get('search');
@@ -24,7 +24,7 @@ export default Component.extend({
   init() {
     this._super();
 
-    let timer = 10;
+    let timer = 11;
 
     setTimeout(() => {
       Ember.$('#timer').show();
@@ -34,10 +34,11 @@ export default Component.extend({
         if (timer === -1) {
           Ember.$('#timer').hide();
           $('.element').fadeOut('slow');
+          this.set('introMsg', false);
         }
-      }, 1150);
-
-    }, 2900);
+      }, 1500);
+""
+    }, 3000);
 
   },
 
